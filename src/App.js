be64615e56b017f9df2fe86fec83c245cefa47d6
CIKADA <adel.garipovazn@mail.ru>
2023-11-18 23:34:39 +0300

    Init project

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import {Component} from 'react';
+
+const date1 = new Date(2023, 7, 19, 14, 5);
+const date2 = new Date(2023, 7, 19, 14, 5);
+
+const initialData = [
+  {
+    title: 'Изучить React',
+    desc: 'Да поскорее!',
+    image: '',
+    done: true,
+    createdAt: date1.toLocaleString(),
+    key: date1.getTime()
+  },
+  {
+    title: 'Написать первое React-приложение',
+    desc: 'Список запланированных дел',
+    image: '',
+    done: false,
+    createdAt: date2.toLocaleString(),
+    key: date2.getTime()
+  }
+];
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.data = initialData;
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar is-light">
+          <div className="navbar-brand">
+            <span className="navbar-item is-uppercase">
+              Todos
+            </span>
+          </div>
+        </nav>
+        <main className="content px-6 mt-6">
+          <h1>Todos</h1>
+        </main>
+      </div>
+    )
+  }
+}
